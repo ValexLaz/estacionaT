a9d7e8be81feaff70af5f833e3cfa2f91fd3e737
Valentina <valentina.lazaro@studentambassadors.com>
2024-03-26 14:51:09 -0400

    registro de usuario y api

--- lib/screens_users/create_account_page.dart
+++ lib/screens_users/create_account_page.dart
@@ -1,5 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:map_flutter/screens_gerentes/create_account_gerente.dart';
+import 'package:http/http.dart' as http;
 
 import 'login_screen.dart';
 
@@ -14,18 +15,20 @@ class _SignUpPageState extends State<SignUpPage> {
   late Color myColor;
   late Size mediaSize;
   TextEditingController nameController = TextEditingController();
+  TextEditingController last_nameController = TextEditingController();
+
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   TextEditingController confirmPasswordController = TextEditingController();
   TextEditingController phoneController = TextEditingController();
   FocusNode nameFocusNode = FocusNode();
+  FocusNode last_nameFocusNode = FocusNode();
   FocusNode emailFocusNode = FocusNode();
   FocusNode passwordFocusNode = FocusNode();
   FocusNode confirmPasswordFocusNode = FocusNode();
   FocusNode phoneFocusNode = FocusNode();
   bool obscurePassword = true;
   bool obscureConfirmPassword = true;
-  String vehicleType = 'Automóvil';
 
   @override
   Widget build(BuildContext context) {
@@ -91,6 +94,10 @@ class _SignUpPageState extends State<SignUpPage> {
         _buildInputField(nameController,
             icon: Icons.person, focusNode: nameFocusNode),
         const SizedBox(height: 20),
+        _buildGreyText("Apellido"),
+        _buildInputField(last_nameController,
+            icon: Icons.person, focusNode: last_nameFocusNode),
+        const SizedBox(height: 20),
         _buildGreyText("Correo Electrónico"),
         _buildInputField(emailController,
             icon: Icons.email, focusNode: emailFocusNode),
@@ -98,9 +105,6 @@ class _SignUpPageState extends State<SignUpPage> {
         _buildGreyText("Número Telefónico"),
         _buildInputField(phoneController,
             icon: Icons.phone, focusNode: phoneFocusNode),
-        const SizedBox(height: 20),
-        _buildGreyText("Tipo de Vehículo"),
-        _buildVehicleTypeDropdown(),
         const SizedBox(height: 20),
         _buildGreyText("Contraseña"),
         _buildPasswordInputField(passwordController, passwordFocusNode,
@@ -115,7 +119,6 @@ class _SignUpPageState extends State<SignUpPage> {
         const SizedBox(height: 40),
         _buildSignUpButton(),
         const SizedBox(height: 20),
-        _buildParkingRegistrationButton(),
       ],
     );
   }
@@ -153,44 +156,72 @@ class _SignUpPageState extends State<SignUpPage> {
     );
   }
 
-  Widget _buildVehicleTypeDropdown() {
-    return DropdownButtonFormField<String>(
-      value: vehicleType,
-      items: <String>['Automóvil', 'Motocicleta', 'Cuadratrack', 'Bicicleta']
-          .map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
-      onChanged: (String? newValue) {
-        setState(() {
-          vehicleType = newValue!;
-        });
-      },
-      decoration: const InputDecoration(
-        border: OutlineInputBorder(),
-        prefixIcon: Icon(Icons.directions_car),
-      ),
-    );
-  }
-
   Widget _buildSignUpButton() {
     return ElevatedButton(
-      onPressed: () {
-        // Navegar a CreateAccountPage
-        Navigator.of(context).push(
-          MaterialPageRoute(builder: (context) => LoginPage()),
+      onPressed: () async {
+        // Datos de registro
+        String username = nameController.text.trim();
+        String last_name = last_nameController.text.trim();
+        String email = emailController.text.trim();
+        String password = passwordController.text.trim();
+        String phone = phoneController.text.trim();
+
+        // Crear el cuerpo de la solicitud
+        Map<String, String> requestBody = {
+          'username': username,
+          'last_name': last_name,
+          'email': email,
+          'password': password,
+          'phone': phone,
+        };
+
+        // Realizar la solicitud HTTP
+        final response = await http.post(
+          Uri.parse(
+              'https://estacionatbackend.onrender.com/api/v2/user/signup/'),
+          body: jsonEncode(requestBody),
+          headers: {
+            'Content-Type': 'application/json',
+          },
         );
+        print(response.body);
+        // Verificar el código de respuesta
+        if (response.statusCode == 200) {
+          // Registro exitoso
+          // Puedes manejar aquí la respuesta si la API devuelve algo
+          // Navegar a la página de inicio de sesión o a donde desees
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => LoginPage()),
+          );
+        } else {
+          // Si la solicitud falla, puedes mostrar
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text('Error de registro'),
+                content: Text(
+                    'Hubo un error al registrar el usuario. Por favor, inténtalo de nuevo más tarde.'),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text('Cerrar'),
+                  ),
+                ],
+              );
+            },
+          );
+        }
       },
       style: ElevatedButton.styleFrom(
         backgroundColor: Color(0xFF1b4ee4),
-        shape: const StadiumBorder(),
+        shape: StadiumBorder(),
         elevation: 20,
-        shadowColor: myColor,
-        minimumSize: const Size.fromHeight(60),
+        minimumSize: Size.fromHeight(60),
       ),
-      child: const Text(
+      child: Text(
         "Registrar",
         style: TextStyle(color: Colors.white),
       ),
@@ -201,7 +232,7 @@ class _SignUpPageState extends State<SignUpPage> {
     return InkWell(
       onTap: () {
         Navigator.of(context).push(
-          MaterialPageRoute(builder: (context) => SignUpParkingPage()),
+          MaterialPageRoute(builder: (context) => SignUpPage()),
         );
       },
       child: Center(
@@ -216,8 +247,6 @@ class _SignUpPageState extends State<SignUpPage> {
       ),
     );
   }
-
-
 }
 
 void main() {
