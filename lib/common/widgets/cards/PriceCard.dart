567b7597bfa41b0df2b5b71b62f79daa14edb099
Kendal696 <kevindaniellozano8@gmail.com>
2024-06-18 15:13:44 -0400

    Detalles de parqueo

--- lib/common/widgets/cards/PriceCard.dart
+++ lib/common/widgets/cards/PriceCard.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-
 import '../../../models/Price.dart';
-
-import 'package:flutter/material.dart';
 
 class PriceCard extends StatelessWidget {
   final Price price;
@@ -21,92 +18,120 @@ class PriceCard extends StatelessWidget {
     return Card(
       elevation: 4.0,
       margin: const EdgeInsets.all(8.0),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12), // Bordes redondeados
+      ),
       child: Container(
-        color: Theme.of(context).colorScheme.secondary,
+        decoration: BoxDecoration(
+          color: Colors.white, // Fondo blanco
+          border: Border.all(color: Colors.blue, width: 2.0), // Bordes azules
+          borderRadius: BorderRadius.circular(12), // Bordes redondeados
+        ),
         child: Padding(
-          padding: const EdgeInsets.only(left:16.0,top:16.0,bottom:16.0),
+          padding: const EdgeInsets.all(16.0),
           child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Row(children: [
-                Container(
-                  width: 100,
-                  height: 50,
-                  margin: EdgeInsets.only(bottom: 20),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(12)),
-                  child: Center(
-                      child: Text(
-                    'Bs ${price.price.toStringAsFixed(2)}',
-                    style: TextStyle(
-                      fontSize: 18.0,
-                      color: Colors.green[700],
-                      fontWeight: FontWeight.w500,
-                    ),
-                  )),
-                ),
-                SizedBox(width: 10), // Espacio entre el precio y el texto
-                Expanded(
-                  child: Text(
-                    'Vehículo: ${price.typeVehicle ?? "No especificado"}',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ]),
               Row(
                 children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      if (price.isReservation)
-                        Text(
-                          'Requiere Reservación',
-                          style: TextStyle(
-                              color: Theme.of(context).colorScheme.surface),
+                  Expanded(
+                    child: Text(
+                      'Vehículo: ${price.typeVehicle ?? "No especificado"}',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black, // Texto negro
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 100,
+                    height: 50,
+                    margin: EdgeInsets.only(left: 10),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Bs ${price.price.toStringAsFixed(2)}',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          color: Colors.green[700],
+                          fontWeight: FontWeight.w500,
                         ),
-                      if (price.isPriceHour)
-                        Text('Precio por Hora',
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 8),
+              Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        if (price.isReservation)
+                          Text(
+                            'Requiere Reservación',
                             style: TextStyle(
-                                color: Theme.of(context).colorScheme.surface)),
-                      if (price.isEntryFee)
-                        Text('Cuota de Entrada',
-                            style: TextStyle(
-                                color: Theme.of(context).colorScheme.surface)),
-                      if (price.priceHour != null)
-                        Text(
-                          'Horario : De ${formatTimeOfDay(price.priceHour!.startTime)} a ${formatTimeOfDay(price.priceHour!.endTime)}',
-                          style: TextStyle(
+                              color: Colors.black, // Texto negro
                               fontSize: 16.0,
-                              color: Theme.of(context).colorScheme.surface),
-                        ),
-                    ],
+                            ),
+                          ),
+                        if (price.isPriceHour)
+                          Text(
+                            'Precio por Hora',
+                            style: TextStyle(
+                              color: Colors.black, // Texto negro
+                              fontSize: 16.0,
+                            ),
+                          ),
+                        if (price.isEntryFee)
+                          Text(
+                            'Cuota de Entrada',
+                            style: TextStyle(
+                              color: Colors.black, // Texto negro
+                              fontSize: 16.0,
+                            ),
+                          ),
+                        if (price.priceHour != null)
+                          Text(
+                            'Horario: De ${formatTimeOfDay(price.priceHour!.startTime)} a ${formatTimeOfDay(price.priceHour!.endTime)}',
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.black, // Texto negro
+                            ),
+                          ),
+                      ],
+                    ),
                   ),
                   if (onTap != null)
                     Container(
-                      padding:EdgeInsets.only(left: 20,bottom: 15),
+                      alignment: Alignment.center,
                       child: IconButton(
-                          onPressed: onTap,
-                          icon: Icon(
-                            Icons.arrow_forward_ios,
-                            color: Colors.white,
-                          )),
+                        onPressed: onTap,
+                        icon: Icon(
+                          Icons.arrow_forward_ios,
+                          color: Colors.black, // Color del ícono
+                        ),
+                      ),
                     ),
-                  if (onDelete != null)
-                  Container(
-                    padding:EdgeInsets.only(left: 20,bottom: 15),
-                    child: IconButton(
-                      icon: Icon(Icons.delete, color: Colors.red,size: 30,),
-                      onPressed: onDelete,
-                    ),
-                  )
-                   
                 ],
-              )
+              ),
+              if (onDelete != null)
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: IconButton(
+                    icon: Icon(
+                      Icons.delete,
+                      color: Colors.red,
+                      size: 30,
+                    ),
+                    onPressed: onDelete,
+                  ),
+                ),
             ],
           ),
         ),
